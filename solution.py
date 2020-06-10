6a670a1f71dc5ed10f557756988eafe495f092f2
Gilbert <gilbertduenas@users.noreply.github.com>
2020-06-11 08:44:12 +0900

    Create solution.py
    This is not optimal.

---
+++ solution.py
@@ -0,0 +1,19 @@
+def expression_matter(a, b, c):
+  max = a + b + c
+
+  if a * b * c > max:
+    max = a * b * c
+  
+  if (a + b) * c > max:
+    max = (a + b) * c
+
+  if (a * b) + c > max:
+    max = (a * b) + c
+
+  if a * (b + c) > max:
+    max = a * (b + c)
+
+  if a + (b * c) > max:
+    max = a + (b * c)
+
+  return max
